7a46fd83d51411b4ca03cef63b63c7d30d353d3b
Jared Heidt <heidtjj@clarkson.edu>
2018-10-23 18:25:20 -0400

    Changes to protocol

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@
 #include <sstream> 
 #include <sys/utsname.h>
 #include <sys/time.h>
+#include <sstream>
+#include <unordered_set>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
@@ -18,13 +21,25 @@ using std::endl;
 using std::string;
 using std::vector;
 using std::to_string;
+using std::stringstream;
 
 // Constants 
 #define PROPER_GREETING "HELLO 1.7"
 #define PROPER_GREETING_LEN 9
 #define DATA "DATA"
 #define DATA_LEN 4
-#define CRLF \r\n
+#define CRLF "\r\n"
+#define EOM "\r\n.\r\n"
+#define MESSAGE_ID "MessageId"
+#define MESSAGE_ID_LEN 9
+#define SUCCESS "SUCCESS"
+#define SUCCESS_LEN 7
+#define WARNING "WARN"
+#define WARNING_LEN 4
+#define QUIT "QUIT"
+#define QUIT_LEN 4
+#define GOODBYE "GOODBYE"
+#define GOODBYE_LEN 7
 
 // Utility functions
 uint16_t checksum(void *data, size_t size);
@@ -32,11 +47,27 @@ vector<string> getIpAndPort(const string& hostName);
 bool receiveGreeting(const int& connectSocket, bool server);
 
 // Server/Client
-void serverFunction(const string& sourceIP, const int& sourcePort, const string& desinationIP, const int& destinationPort, const bool originator);
-void clientFunction(const string& sourceIP, const int& sourcePort, const string& desinationIP, const int& destinationPort, const bool originator);
-void initializeMessage(string& transmitMessage, const string& fromHost, const string& toHost);
+string serverFunction(const string& sourceIP, const int& sourcePort, const string& desinationIP, const int& destinationPort, const bool originator);
+void clientFunction(const string& sourceIP, const int& sourcePort, const string& desinationIP, const int& destinationPort, const bool originator, string& message);
+void initializeMessage(string& transmitMessage, const string& fromHost, const string& fromPort, const string& toHost, const string& toPort);
 
-// System name
+// Retreives the data of a header in a message.
+string getHeaderData(const string& message, const string& headerName){
+    if(message.empty()) return message;
+    size_t headerPosition = message.find(headerName);
+    if(headerPosition == string::npos) return "";
+    size_t endOfHeaderPosition = message.find(CRLF, headerPosition+1);
+    // +2 for ':' and ' '
+    return message.substr(headerPosition + headerName.length()+2, endOfHeaderPosition-(headerPosition + headerName.length()+2));
+}
+
+string getMessageData(const string& message){
+    if(message.empty()) return message;
+    size_t startOfMessage = message.find("\r\n\r\n")+4;
+    if(startOfMessage == string::npos) return "";
+    size_t endOfMessage = message.length()-5;
+    return message.substr(startOfMessage, endOfMessage - startOfMessage);
+}
 
 int main(int argc, char* argv[]){
     if(argc != 4){
@@ -70,20 +101,29 @@ int main(int argc, char* argv[]){
     string destinationIP = destIpPortPair[0];
     int destPort = stoi(destIpPortPair[1]);
 
+    string transmitMessage = "";
     if(originator == "1"){
         // user is originator
-        string transmitMessage = "";
-        initializeMessage(transmitMessage, sourceIP, destinationIP);
-        clientFunction(sourceIP, sourcePort, destinationIP, destPort, true);
-        serverFunction(sourceIP, sourcePort, destinationIP, destPort, true);
+        clientFunction(sourceIP, sourcePort, destinationIP, destPort, true, transmitMessage);
+        string received = serverFunction(sourceIP, sourcePort, destinationIP, destPort, true);
     }
     else {
         // user is not originator
-        serverFunction(sourceIP, sourcePort, destinationIP, destPort, false);
-        clientFunction(sourceIP, sourcePort, destinationIP, destPort, false);
+        string received = serverFunction(sourceIP, sourcePort, destinationIP, destPort, false);
+        clientFunction(sourceIP, sourcePort, destinationIP, destPort, false, transmitMessage);
     }
 
     return 0;
+}
+
+// **************************************************************************************************
+
+template< typename T >
+std::string int_to_hexStr( T i ) {
+  std::stringstream stream;
+  stream << std::setfill ('0') << std::setw(sizeof(T)*2) 
+         << std::hex << i;
+  return stream.str();
 }
 
 // **************************************************************************************************
@@ -172,8 +212,8 @@ bool sendDATAString(const int& connectSocket){
     data will be sent next from client. 
  */
 bool receiveDATAString(const int& connectSocket){
-    char buffer[1024] = {0}; 
-    int numBytesRead = read(connectSocket, buffer, 1024);
+    char buffer[DATA_LEN+1] = {'\0'}; 
+    int numBytesRead = read(connectSocket, buffer, DATA_LEN);
     cout << "Server read: " << buffer << " -> " << numBytesRead << endl;
     if(numBytesRead != DATA_LEN){
         perror("DATA string unsuccessfully received."); 
@@ -181,8 +221,6 @@ bool receiveDATAString(const int& connectSocket){
     }
 
     string greeting = buffer;
-    memset(buffer, '\0', 1024); 
-
     if(greeting != DATA){
         send(connectSocket, "QUIT", 4, 0); 
         perror("DATA string unsuccessfully received."); 
@@ -194,8 +232,8 @@ bool receiveDATAString(const int& connectSocket){
 // **************************************************************************************************
 
 bool receiveGreeting(const int& connectSocket, bool server){
-    char buffer[1024] = {0}; 
-    int numBytesRead = read(connectSocket, buffer, 1024);
+    char buffer[PROPER_GREETING_LEN+1] = {'\0'};
+    int numBytesRead = read(connectSocket, buffer, PROPER_GREETING_LEN);
     if(server)
         cout << "server read: " << buffer << " -> " << numBytesRead << endl;
     else 
@@ -215,13 +253,95 @@ bool receiveGreeting(const int& connectSocket, bool server){
         close(connectSocket); 
         return false; 
     }
-    memset(buffer, '\0', 1024);
     return true;
 }
 
 // **************************************************************************************************
 
-void serverFunction(const string& sourceIP, const int& sourcePort, 
+string validateHeader(const string& message){
+    string errorMessage = "";
+    cout << "Server read: " << message << endl;
+
+    // Validate checksum.
+    string receivedCheckSumStr = getHeaderData(message, "MessageChecksum");
+    string messageData = getMessageData(message);
+    // Recompute checksum.
+    uint16_t actualCheckSum = checksum((void*)messageData.c_str(), messageData.length()); 
+    string actualCheckSumStr = int_to_hexStr(actualCheckSum);
+    // Compare checksums.
+    if(actualCheckSumStr != receivedCheckSumStr
+            || actualCheckSumStr.length() != 4
+            || receivedCheckSumStr.length() != 4){
+        errorMessage += "Checksum is not valid at hop " + getHeaderData(message, "Hop") +  + CRLF;
+    }
+
+    // Check for all valid headers.
+    // Hop
+    if(getHeaderData(message, "Hop").empty())
+        errorMessage += "Warning: Missing required header - Hop" + CRLF;
+
+    // MessageId
+    if(getHeaderData(message, "MessageId").empty())
+        errorMessage += "Warning: Missing required header - MessageId" + CRLF;
+    
+    // FromHost
+    if(getHeaderData(message, "FromHost").empty())
+        errorMessage += "Warning: Missing required header - FromHost" + CRLF;
+    
+    // ToHost
+    if(getHeaderData(message, "ToHost").empty())
+        errorMessage += "Warning: Missing required header - ToHost" + CRLF;
+    
+    // System
+    if(getHeaderData(message, "System").empty())
+        errorMessage += "Warning: Missing required header - System" + CRLF;
+    
+    // Program
+    if(getHeaderData(message, "Program").empty())
+        errorMessage += "Warning: Missing required header - Program" + CRLF;
+    
+    // Author
+    if(getHeaderData(message, "Author").empty())
+        errorMessage += "Warning: Missing required header - Author" + CRLF;
+    
+    // SendingTimestamp
+    if(getHeaderData(message, "SendingTimestamp").empty())
+        errorMessage += "Warning: Missing required header - SendingTimestamp" + CRLF;
+    
+    // MessageChecksum
+    if(getHeaderData(message, "MessageChecksum").empty())
+        errorMessage += "Warning: Missing required header - MessageChecksum" + CRLF;
+    
+    // Search for duplicate messageId
+    std::unordered_set<string> messageIds;
+    size_t start = 0;
+    while(start >= 0 && start < message.length()){
+        // Find start index of the next message id.
+        start = message.find(MESSAGE_ID, start);
+        if(start == string::npos) break;
+        // Find the end index of this header.
+        size_t endOfHeaderPosition = message.find(CRLF, start+1);
+        // +1 for ':' and +1 ' '
+        string id = message.substr(start + MESSAGE_ID_LEN+2, endOfHeaderPosition-(start + MESSAGE_ID_LEN+2));
+        
+        // Check for duplicated ID.
+        if(messageIds.find(id) != messageIds.end()){
+            errorMessage += "Warning: Duplicate message ID - " + id + CRLF
+            break;
+        }
+        // Insert current id into the messageIds set.
+        messageIds.insert(id);
+        ++start;
+    }
+
+    cout << "Error Message: " << errorMessage << endl;
+    cout << "End of validation" << endl;
+    return errorMessage;
+}
+
+// **************************************************************************************************
+
+string serverFunction(const string& sourceIP, const int& sourcePort, 
                         const string& desinationIP, const int& destinationPort, const bool originator){
     int doorbellSocket;
     int connectSocket;
@@ -278,23 +398,58 @@ void serverFunction(const string& sourceIP, const int& sourcePort,
         // Handle "DATA" not properly received.
         cout << "DATA not properly recieved from server." << endl;
     }
+    // Read data from client
+    char buffer[1024] = {'\0'}; 
+    read(connectSocket, buffer, 1024);
+    string buff = buffer;
     
+    // Validate headers.
+    string errorMessage = validateHeader(buff);
+    if(errorMessage.empty()){
+        send(connectSocket, SUCCESS, SUCCESS_LEN, 0);
+        cout << "Server sent SUCCESS" << endl;
+    }
+    else {
+        buff = "Warning: " + errorMessage + buff; 
+        send(connectSocket, WARNING, WARNING_LEN, 0); 
+        cout << "Server sent WARNING" << endl;
+    }
 
-    cout << "Server done" << endl << endl;
+    // Read QUIT from client.
+    read(connectSocket, buffer, QUIT_LEN);
+    cout << "Server read QUIT" << endl;
+
+    // Send GOODBYE to client.
+    send(connectSocket, GOODBYE, GOODBYE_LEN, 0); 
+    cout << "Server sent GOODBYE" << endl;
+
+    // Close connection socket.
     close(connectSocket);
+    cout << "Server closed connection." << endl << endl;
+
+    return buff;
 }
 
 // **************************************************************************************************
 
 void clientFunction(const string& sourceIP, const int& sourcePort, 
-                        const string& desinationIP, const int& destinationPort, const bool originator){
+                        const string& desinationIP, const int& destinationPort, 
+                        const bool originator, string& message){
+
+    if(originator){
+        initializeMessage(transmitMessage, sourceIP, sourceIpPortPair[1], destinationIP, destIpPortPair[1]);
+    }
+    else {
+
+    }
+    
     int connectSocket;
     sockaddr_in serverAddress; 
     if ((connectSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
         cout << "Socket creation error" << endl; 
         exit(EXIT_FAILURE);
-    } 
+    }
 
     memset(&serverAddress, '0', sizeof(serverAddress)); 
    
@@ -318,7 +473,7 @@ void clientFunction(const string& sourceIP, const int& sourcePort,
         close(connectSocket);
         exit(EXIT_FAILURE);
     }
-
+    // START OF HANDSHAKE
     // Connected to server socket. Wait for greeting.
     bool success = receiveGreeting(connectSocket, false);
     if(!success) exit(EXIT_FAILURE);// Error is already printed.
@@ -326,8 +481,10 @@ void clientFunction(const string& sourceIP, const int& sourcePort,
     // Acknowledge server by sending back greeting.
     success = sendGreeting(connectSocket, 0);
     if(!success) exit(EXIT_FAILURE);// Error is already printed.
-    
-    // Send "DATA" to server.
+    // END OF HANDSHAKE
+
+    // START OF DATA TRANSFER
+    // Send "DATA" string to server.
     success = sendDATAString(connectSocket);
     if(!success) {
         // Handle "DATA" not able to be sent.
@@ -335,24 +492,40 @@ void clientFunction(const string& sourceIP, const int& sourcePort,
         cout << "DATA could not be sent from client to server." << endl;
         exit(EXIT_FAILURE);
     }
-/*
-    // Send actual data to server. Start with headers.
-    string curHeader = "Hop: ";
-    if(originator) curHeader += 
-    send(connectSocket, PROPER_GREETING, PROPER_GREETING_LEN, 0 ); 
-*/
-    close(connectSocket);
 
-    cout << "Client done" << endl<< endl;
+    // Send actual data to server. Start with headers.
+    send(connectSocket, message.c_str(), message.length(), 0 ); 
+    cout << endl << "Client sent: " << message << endl;
+
+    // Read either SUCCESS or WARN from server.
+    char buffer[SUCCESS_LEN+1] = {'\0'}; 
+    read(connectSocket, buffer, SUCCESS_LEN);
+    cout << "Client read " << buffer << endl;
+    
+    // Send QUIT to server.
+    send(connectSocket, QUIT, QUIT_LEN, 0); 
+    cout << "Client sent " << QUIT << endl;
+    
+    // Reset buffer
+    memset(buffer, '\0', SUCCESS_LEN+1);
+
+    // Read either SUCCESS or WARN from server.
+    read(connectSocket, buffer, GOODBYE_LEN);
+    cout << "Client read " << GOODBYE << endl;
+
+    int val = close(connectSocket);
+    cout << "Client closed connection: " << val << endl;
 }
 
+// **************************************************************************************************
+
+// current UTC date/time based on current system
 string getCurrentTimestamp(){
-    // current date/time based on current system
     timeval tvTime;
     gettimeofday(&tvTime, NULL);
 
     int iTotal_seconds = tvTime.tv_sec;
-    struct tm *ptm = localtime((const time_t *) & iTotal_seconds);
+    struct tm *ptm = gmtime((const time_t *) & iTotal_seconds);
 
     int iHour = ptm->tm_hour;;
     int iMinute = ptm->tm_min;
@@ -361,27 +534,38 @@ string getCurrentTimestamp(){
     return to_string(iHour) + ":" + to_string(iMinute) + ":" + to_string(iSecond) + ":" + to_string(iMilliSec);
 }
 
-void initializeMessage(string& transmitMessage, const string& fromHost, const string& toHost){
-	
+// **************************************************************************************************
+
+string getHeaders(const string& fromHost, const string& fromPort, const string& toHost, const string& toPort){
     // Get system name.
     utsname name;
     if(uname(&name)) exit(-1);
     string sysname = name.sysname;
-    string sysrelease = name.sysname;
+    string sysrelease = name.release;
     
     string timestamp = getCurrentTimestamp();
+    string headers = "";
+    headers += "Hop: 0\r\n";
+    headers += "MessageId: 1234\r\n";
+    headers += "FromHost: " + fromHost + ":" + fromPort + CRLF;
+    headers += "ToHost: " + toHost + ":" + toPort + CRLF;
+    headers += "System: " + sysname + "/" + sysrelease + CRLF;
+    headers += "Program: C++11/GCC\r\n";
+    headers += "Author: Jared Heidt\r\n";
+    headers += "SendingTimestamp: " + timestamp + CRLF;
+    return headers;
+}
 
-    transmitMessage += "Hop: 0\r\n";
-    transmitMessage += "MessageId: 1234\r\n";
-    transmitMessage += "FromHost: " + fromHost + "\r\n";
-    transmitMessage += "ToHost: " + toHost + "\r\n";
-    transmitMessage += "System: " + sysname + "/" + sysrelease + "\r\n";
-    transmitMessage += "Program: C++11/GCC\r\n";
-    transmitMessage += "Author: Jared Heidt\r\n";
-    transmitMessage += "SendingTimestamp: " + timestamp + "\r\n";
+// **************************************************************************************************
 
+void initializeMessage(string& transmitMessage, const string& fromHost, const string& fromPort, const string& toHost, const string& toPort){
+    string headers = getHeaders(fromHost, fromPort, toHost, toPort);
+    transmitMessage = headers;
+    transmitMessage += "Hello! You're receiving a message from the telephone game!";
 
-	printf("Your computer's OS is %s@%s\n", name.sysname, name.release);
-
-
+    uint16_t checkSum = checksum((void*)message.c_str(), message.length());
+    transmitMessage += "MessageChecksum: " + int_to_hexStr(checkSum) + CRLF;
+    transmitMessage += CRLF;
+    transmitMessage += message;
+    transmitMessage += EOM;
 }
